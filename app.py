dbf161838c85ffb0739dba3a0705b44e89d0a631
Ananya Kantareddy <kanta344@users.noreply.huggingface.co>
2026-07-28 14:31:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import gradio as gr
 from huggingface_hub import InferenceClient
+from sentence_transformers import SentenceTransformer  #library that's necessary to tconvert text into vector representation (embeddings) that capture meaning
+import torch #pytorch: allows us to work with tensors
 
 # This is the same pattern from the Generative AI lesson! It uses the
 # Inference Provider API to send your messages to an AI model and get
@@ -9,6 +11,13 @@ from huggingface_hub import InferenceClient
 # Note: if this Space doesn't already have one, you'll need to add an
 # HF_TOKEN secret in the Space's Settings tab for this to work
 # (Settings -> Variables and secrets -> New secret).
+with open("water_cycle.txt", "r", encoding="utf-8") as file:                 #open water_cycle text in "read" mode w/ encoding utf-8
+  # Read the entire contents of the file and store it in a variable
+  water_cycle_text = file.read()                                                
+
+# Print the text below
+print(water_cycle_text)
+
 
 client = InferenceClient("Qwen/Qwen2.5-7B-Instruct")
 
