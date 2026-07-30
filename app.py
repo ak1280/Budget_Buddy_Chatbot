60a66b2cb0c2c22eb6913dbb238a37190020cbef
Emily Suriel <emsuri@users.noreply.huggingface.co>
2026-07-30 15:28:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -144,15 +144,16 @@ with gr.Blocks(theme=custom_theme) as chatbot:
     gr.Image(display_image())
 
     gr.HTML("""
-    <title style="color:#042107; font-weight:bold; text-align:center;">
-        Introducing the Budget Buddy! 💵
-    </title>
-    """)
-
-    gr.HTML("""
-    <h2 style= "color:#042107; font-weight:bold; text-align:center;">
-        This tool simplifies financial literacy and helps young adults make informed financial decisions. 💸
-    </h2>
+    <head>
+        <title style="color:#042107; font-weight:bold; text-align:center;">
+            Introducing the Budget Buddy! 💵
+        </title>
+    </head>
+    <body>
+        <h2 style= "color:#042107; font-weight:bold; text-align:center;">
+            This tool simplifies financial literacy and helps young adults make informed financial decisions. 💸
+        </h2>
+    </body>
     """)
     
     gr.ChatInterface(
